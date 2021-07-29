0dd01a8ac7ac1577bd98d3fa2a1aedbccbaf0b95
hafifiomar <mohamadhafifiomar@gmail.com>
2021-07-29 15:10:29 +0800

    push latest updated files

--- src/assets/js/partials/_button-toggle.js
+++ src/assets/js/partials/_button-toggle.js
@@ -9,3 +9,25 @@ $('.select-room').click(function(e){
         $(this).closest('.room-list__wrapper').addClass("selected--");
     }
 })
+
+var scrollHeight = true;
+
+if($(".room-reservation__wrapper").length > 0) {
+    $(window).on('scroll.paymentBreakdown resize.paymentBreakdown', function() {
+        if (scrollHeight == false) {
+            return;
+        } if ($(window).scrollTop() + $(window).outerHeight() >= $(".room-reservation__details-wrapper").offset().top) {
+            $(".room-reservation__wrapper").addClass("toggle-payment-header");
+        } else {
+            $(".room-reservation__wrapper").removeClass("toggle-payment-header");
+        }
+    });
+}
+
+$(window).triggerHandler('scroll.paymentBreakdown');
+
+$(".room-reservation__mobile").click(function (){
+    $('html, body').animate({
+        scrollTop: $(".room-reservation__wrapper").offset().top
+    }, 300);
+});
